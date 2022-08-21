b6d91ab35760f03f97eb8913c1107919cba9181f
iSebasC <sebas.cabrera.456@gmail.com>
2022-08-20 23:44:11 -0500

    Mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import comic1 from "./assets/comic1.jpg";
+import comic2 from "./assets/comic2.jpg";
+import comic3 from "./assets/comic3.jpg";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [index, setIndex] = useState(0);
+
+  const cards = [
+    { id: "1", imagen: comic1 },
+    { id: "2", imagen: comic2 },
+    { id: "3", imagen: comic3 },
+  ]
+
+  const mod = (n, m) => {
+    let result = n % m;
+    //Devolver un valor positivo
+    return result >= 0 ? result : result + m;
+       
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIndex((index + 1) % cards.length);
+    } , 3000);
+  }, [index]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="carousel">
+
+        {cards.map((item, i) => {
+          const indexleft = mod(index - 1, cards.length);
+          const indexright = mod(index + 1, cards.length);
+
+          let className = "";
+
+          if (i === index) {
+            className = "card card--active";
+          } else if (i === indexright) {
+            className = "card card--right";
+          } else if (i === indexleft) {
+            className = "card card--left";
+          }   
+
+          return <img key={item.id} src={item.imagen} alt="Comic" className={className} />
+
+        })}
+            
+      </div>
     </div>
   );
 }
